6361588405a2e4bcbb1f8ebc9cc318488411695a
Jason Pau <jason@jasonpau.com>
2017-07-01 22:15:44 -0700

    Initial commit...main html structure in place

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=devide-width, initial-scale=1" />
+  <title>Jason Pau - Web Developer</title>
+  <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,700" rel="stylesheet">
+  <link rel="stylesheet" href="css/styles.css" />
+</head>
+<body>
+  <header>
+    <div id="header">
+      <div class="logo">
+        Jason Pau Development
+      </div>
+      <nav>
+        <a href="#portfolio">Portfolio</a>
+        <a href="#portfolio">About</a>
+        <a href="#portfolio">Contact</a>
+      </nav>
+    </div>
+  </header>
+
+  <main>
+    <div id="welcome">
+      <section>
+        <h1>Hi! I'm Jason, and I'm a web developer.</h1>
+      </section>
+    </div>
+
+    <div id="portfolio">
+      <section id="portfolio">
+        <h1>Portfolio</h1>
+      </section>
+    </div>
+
+    <div id="about">
+      <section id="about">
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A eius eos ex magnam minima praesentium quae quas quisquam reprehenderit sunt! Atque cupiditate, delectus deserunt eligendi facere fugit id illum iste laboriosam nemo officia quaerat quasi quod recusandae repellendus, sequi sunt ut! Aperiam distinctio nihil odio sapiente tenetur? Ab adipisci, et harum incidunt numquam perspiciatis quae quam similique. Aliquam amet at delectus dignissimos dolores excepturi expedita in ipsa, iusto nam natus nisi nulla numquam provident quam, quas quasi quidem reiciendis rem saepe sint totam voluptas. Asperiores et, explicabo fugit harum modi nam nihil odio odit, optio quos, sint totam velit voluptatibus.</p>
+        <aside>
+          Icons for various tech skills go here...
+        </aside>
+        <a href="assets/jason_pau_resume.pdf" target="_blank">Learn More</a>
+      </section>
+    </div>
+
+    <div id="contact">
+      <section id="contact">
+        <h1>Contact</h1>
+      </section>
+    </div>
+  </main>
+
+  <footer>
+    <div id="footer">
+      <div>Find me on social media...</div>
+      <div>Phone and email</div>
+    </div>
+  </footer>
+</body>
+</html>
